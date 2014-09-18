7d593d8482f6740d7528db2cbd0f7cb53fd7f2ff
Peter Mösenthin <peter.moesenthin@gmail.com>
2014-09-19 00:37:22 +0200

    Removed useless comment

--- app/src/main/java/de/petermoesenthin/alarming/fragments/SettingsFragment.java
+++ app/src/main/java/de/petermoesenthin/alarming/fragments/SettingsFragment.java
@@ -28,8 +28,6 @@ public class SettingsFragment extends android.preference.PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
     }
 
