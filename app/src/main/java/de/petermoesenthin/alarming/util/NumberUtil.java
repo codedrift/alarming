acca2fa03a0c81e21d8e42cd55074efaa2ff049c
Peter Mösenthin <peter.moesenthin@gmail.com>
2014-09-23 18:08:40 -0500

    Added numberutil and function to convert a long to int with max range

---
+++ app/src/main/java/de/petermoesenthin/alarming/util/NumberUtil.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2014 Peter Mösenthin <peter.moesenthin@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.petermoesenthin.alarming.util;
+
+import android.util.Log;
+
+public class NumberUtil {
+
+    public static final String DEBUG_TAG = "NumberUtil";
+    private static final boolean D = false;
+
+
+    /**
+     * Parses a long number to an integer and caps it to MIN/MAX Integer values
+     * @param number
+     * @return
+     */
+    public static int parseLongToCappedInt(long number){
+        if (D) { Log.d(DEBUG_TAG, "Parsing Long to int");}
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+        if (number >= maxInt){
+            return maxInt;
+        } else if (number <= minInt){
+            return minInt;
+        } else {
+            return (int) number;
+        }
+    }
+
+
+}
