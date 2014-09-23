aea413d1da34a53f2e3025edc9f1f95b9a146761
Peter Mösenthin <peter.moesenthin@gmail.com>
2014-09-23 17:13:38 -0500

    Added ID3 meta data read to MediaPlayerUtil

--- app/src/main/java/de/petermoesenthin/alarming/util/MediaPlayerUtil.java
+++ app/src/main/java/de/petermoesenthin/alarming/util/MediaPlayerUtil.java
@@ -1,7 +1,59 @@
+/*
+ * Copyright (C) 2014 Peter Mösenthin <peter.moesenthin@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.petermoesenthin.alarming.util;
 
-/**
- * Created by root on 9/22/14.
- */
+import android.media.MediaMetadataRetriever;
+import android.util.Log;
+
 public class MediaPlayerUtil {
+
+    public static final String DEBUG_TAG = "MediaPlayerUtil";
+    private static final boolean D = true;
+
+
+    /**
+     * Creates an array of basic information about an audio file obtained through the
+     * MediaMetaDataRetriever.
+     * The set contains information as follows:
+     * [0] METADATA_KEY_ARTIST
+     * [1] METADATA_KEY_TITLE
+     * [2] METADATA_KEY_DURATION
+     * @param filePath path to the file
+     * @return
+     */
+    public static String[] getBasicMetaData(String filePath){
+        if (D) {Log.d(DEBUG_TAG, "Reading audio metadata");}
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+        mmr.setDataSource(filePath);
+        String[] metaData = new String[3];
+        metaData[0] =
+                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+        metaData[1] =
+                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        metaData[2] =
+                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        if (D) {Log.d(DEBUG_TAG, "Found:" +
+                " [0] " + metaData[0] +
+                " [1] " + metaData[1] +
+                " [2] " + metaData[2]);}
+        return metaData;
+    }
+
+
+
+
 }
