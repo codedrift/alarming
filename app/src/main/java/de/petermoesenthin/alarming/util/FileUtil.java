5de8aae3684e35f93bc780d1d7aa24d3bedf1011
Peter Mösenthin <peter.moesenthin@gmail.com>
2014-09-23 17:13:13 -0500

    Added comments and new methods to fileutil

--- app/src/main/java/de/petermoesenthin/alarming/util/FileUtil.java
+++ app/src/main/java/de/petermoesenthin/alarming/util/FileUtil.java
@@ -21,6 +21,12 @@ import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
+
+import com.google.gson.Gson;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -28,6 +34,9 @@ import java.io.FileFilter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+
+import de.petermoesenthin.alarming.pref.AlarmSoundGson;
 
 public class FileUtil {
 
@@ -35,7 +44,13 @@ public class FileUtil {
     private static final boolean D = false;
 
     public static final String APP_EXT_STORAGE_FOLDER = "alarming";
+    public static final String AUDIO_METADATA_FILE_EXTENSION = ".alarmingmeta";
 
+    /**
+     * Copies a file to the Alarming directory in the external storage
+     * @param context Application context
+     * @param uri Uri of file to copy
+     */
     public static void saveFileToExtAppStorage(final Context context, final Uri uri){
         final File applicationDirectory = getApplicationDirectory();
         if(!applicationDirectory.exists()){
@@ -46,13 +61,13 @@ public class FileUtil {
         if(!noMedia.exists()){
             try {
                 noMedia.createNewFile();
-                if (D) {Log.e(DEBUG_TAG, "Created .nomedia file in: "  + noMedia.getAbsolutePath());}
+                if (D) {Log.e(DEBUG_TAG, "Created .nomedia file in: " + noMedia.getAbsolutePath());}
             } catch (IOException e) {
                 if (D) Log.e(DEBUG_TAG, "Unable to create .nomedia file", e);
             }
         }
 
-        String fileName = getFilenameFromUri(uri);
+        String fileName = getFilenameFromUriNoSpace(uri);
         final File destinationFile = new File(applicationDirectory.getPath() + File.separatorChar
                 + fileName);
 
@@ -94,6 +109,10 @@ public class FileUtil {
         copyThread.start();
     }
 
+    /**
+     * Checks if the external storage is writeable
+     * @return
+     */
     public static boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED.equals(state)) {
@@ -102,6 +121,10 @@ public class FileUtil {
         return false;
     }
 
+    /**
+     * Checks if the external storage is readable
+     * @return
+     */
     public static boolean isExternalStorageReadable() {
         String state = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED.equals(state) ||
@@ -111,15 +134,23 @@ public class FileUtil {
         return false;
     }
 
-    public static String getFilenameFromUri(Uri uri){
+    /**
+     * Returns the filename from a given path and replaces all spaces with underscores
+     * @param uri
+     * @return
+     */
+    public static String getFilenameFromUriNoSpace(Uri uri){
         String result = null;
         String path = uri.getPath();
         File f = getFile(path);
-        // Replace all space chars with underscores
         result = f.getName().replaceAll("\\s", "_");
         return result;
     }
 
+    /**
+     * Deletes a file on external storage
+     * @param path
+     */
     public static void deleteFile(String path){
         File file = new File(path);
         if(file.delete()){
@@ -129,25 +160,89 @@ public class FileUtil {
         }
     }
 
+    /**
+     * @param path Path of file to get
+     * @return File with given path
+     */
     public static File getFile(String path){
         return new File(path);
     }
 
+    /**
+     * @return Application directory in external storage
+     */
     public static File getApplicationDirectory(){
         return new File(Environment.getExternalStorageDirectory()
                 .getPath() + File.separatorChar + APP_EXT_STORAGE_FOLDER + File.separatorChar);
     }
 
+    /**
+     * Returns a file list of all non-hidden files in the application directory.
+     * @return
+     */
     public static File[] getAlarmDirectoryFileList(){
-        File[] files = FileUtil.getApplicationDirectory().listFiles(new FileFilter() {
+        return FileUtil.getApplicationDirectory().listFiles(new FileFilter() {
             @Override
             public boolean accept(File f) {
                 return !f.isHidden();
             }
         });
-        return files;
     }
 
+    /**
+     *  Returns a file list of all non-hidden audio files in the application directory.
+     * @return
+     */
+    public static File[] getAlarmDirectoryAudioFileList(){
+        return FileUtil.getApplicationDirectory().listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File f) {
+                boolean fileAccepted = false;
+                // Check if is hidden
+                fileAccepted = !f.isHidden();
+                // Check for mime type
+                String mime = getMimeType(f.getAbsolutePath());
+                if(!mime.startsWith("audio")){
+                    fileAccepted = true;
+                }
+                return fileAccepted;
+            }
+        });
+    }
+
+    /**
+     * Writes the sound configuration file based on
+     * @param soundFilePath the path of the used sound file and
+     * @param alsg the configuration in form of a AlarmSoundGson object
+     */
+    public static void writeSoundConfigurationFile(String soundFilePath, AlarmSoundGson alsg){
+        String configFilePath =
+                FilenameUtils.removeExtension(soundFilePath) + AUDIO_METADATA_FILE_EXTENSION;
+        File configFile = getFile(configFilePath);
+        Gson gs = new Gson();
+        String js = gs.toJson(alsg);
+        try {
+            FileUtils.write(configFile, js, "UTF-8");
+        } catch (IOException e) {
+            if (D) {Log.e(DEBUG_TAG, "Could not write audio configuration file",e);}
+        }
+    }
+
+    /**
+     * Reads the
+     * @param soundFilePath
+     * @return
+     */
+    public static AlarmSoundGson readSoundConfigurationFile(String soundFilePath){
+        //TODO implement
+        return null;
+    }
+
+    /**
+     * Returns a mime type for a file in given path
+     * @param url
+     * @return
+     */
     public static String getMimeType(String url){
         String type = null;
         String extension = MimeTypeMap.getFileExtensionFromUrl(url);
