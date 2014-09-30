b34075b8fb88668dea6f6445de1f5693fe46ee7d
Peter Mösenthin <peter.moesenthin@gmail.com>
2014-09-30 17:17:45 -0500

    Added ListView and info dialog to AboutActivity. Doesn't show anything yet

--- app/src/main/java/de/petermoesenthin/alarming/AboutActivity.java
+++ app/src/main/java/de/petermoesenthin/alarming/AboutActivity.java
@@ -1,13 +1,39 @@
+/*
+ * Copyright (C) 2014 Peter Mösenthin <peter.moesenthin@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.petermoesenthin.alarming;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class AboutActivity extends Activity {
+
+    private ListView mListView;
+    public static final String DEBUG_TAG = "AboutActivity";
+    private static final boolean D = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +41,9 @@ public class AboutActivity extends Activity {
         setContentView(R.layout.activity_about);
         this.getActionBar().setHomeButtonEnabled(true);
         this.getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mListView = (ListView) findViewById(R.id.listView_about);
+        setUpListView();
     }
 
     @Override
@@ -29,5 +58,42 @@ public class AboutActivity extends Activity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+
+    private void showInfoDialog(String title, int textRes){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title).setMessage(textRes)
+                .setCancelable(false)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.dismiss();
+                            }
+                        }
+                );
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void setUpListView(){
+        if (D) {Log.d(DEBUG_TAG, "Setting up listView");}
+        final String[] aboutTitles = getResources().getStringArray(R.array.about_actions);
+        mListView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
+                aboutTitles));
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                switch (position){
+                    // About
+                    case 0:
+                        showInfoDialog(aboutTitles[0], R.string.about_this_app);
+                        break;
+                    // Licenses
+                    case 1:
+                        showInfoDialog(aboutTitles[1], R.string.licenses);
+                        break;
+                }
+            }
+        });
     }
 }
