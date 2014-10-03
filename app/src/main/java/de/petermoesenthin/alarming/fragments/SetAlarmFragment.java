5e051759629ece9be2f5bc745562dd4facf09d66
Peter Mösenthin <peter.moesenthin@gmail.com>
2014-10-02 19:40:03 -0500

    Replaced Switch with CircleButton

--- app/src/main/java/de/petermoesenthin/alarming/fragments/SetAlarmFragment.java
+++ app/src/main/java/de/petermoesenthin/alarming/fragments/SetAlarmFragment.java
@@ -16,27 +16,30 @@
 
 package de.petermoesenthin.alarming.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.doomonafireball.betterpickers.timepicker.TimePickerBuilder;
 import com.doomonafireball.betterpickers.timepicker.TimePickerDialogFragment;
 
 import java.util.Calendar;
 
+import at.markushi.ui.CircleButton;
+import de.petermoesenthin.alarming.AlarmReceiverActivity;
 import de.petermoesenthin.alarming.R;
 import de.petermoesenthin.alarming.pref.AlarmGson;
-import de.petermoesenthin.alarming.pref.PrefKey;
 import de.petermoesenthin.alarming.util.AlarmUtil;
 import de.petermoesenthin.alarming.util.PrefUtil;
 import de.petermoesenthin.alarming.util.StringUtil;
@@ -50,13 +53,15 @@ public class SetAlarmFragment extends Fragment implements
     // Members
     //================================================================================
 
-    Calendar mCalendarSet = null;
+    private Calendar mCalendarSet = null;
 
     public static final String DEBUG_TAG = "SetAlarmFragment";
     private static final boolean D = true;
 
+    private Context fragmentContext;
+
     private TextView textView_alarmTime;
-    private Switch switch_alarm;
+    private CircleButton circleButton;
 
     //================================================================================
     // Lifecycle
@@ -65,13 +70,13 @@ public class SetAlarmFragment extends Fragment implements
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if(D) {Log.d(DEBUG_TAG,"onCreateView()");}
+        fragmentContext = getActivity();
         View rootView = inflater.inflate(R.layout.fragment_setalarm, container, false);
-
-        //Create the card
-        Card card = new Card(getActivity(),R.layout.card_alarmtime);
+        // Card view
+        Card card = new Card(fragmentContext, R.layout.card_alarmtime);
         CardView cardView = (CardView) rootView.findViewById(R.id.alarmCard);
         cardView.setCard(card);
-
         textView_alarmTime = (TextView) cardView.findViewById(R.id.textView_alarmTime);
         textView_alarmTime.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,78 +84,102 @@ public class SetAlarmFragment extends Fragment implements
                 showTimePicker();
             }
         });
-        switch_alarm = (Switch) cardView.findViewById(R.id.switch_alarm);
-        setSwitchCheckedListener();
+
+        circleButton = (CircleButton) cardView.findViewById(R.id.button_alarm_set);
+        circleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlarmGson alg = PrefUtil.getAlarmGson(fragmentContext);
+                boolean alarmSet = alg.isAlarmSet();
+                if(alarmSet){
+                    deactivateAlarm();
+                } else {
+                    activateAlarm();
+                }
+
+            }
+        });
+
+        Button alarmTest = (Button) rootView.findViewById(R.id.testlalarm);
+        alarmTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                testAlarm();
+            }
+        });
 
         return rootView;
+    }
+
+    private void testAlarm(){
+        Intent i = new Intent(fragmentContext, AlarmReceiverActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        updateAlarmStateFromPreference();
-        updateSwitchFromPreference();
+        loadAlarmState();
     }
 
     //================================================================================
     // Methods
     //================================================================================
 
-
-    /**
-     * Set the alarm switch state from preference
-     */
-    private void updateSwitchFromPreference() {
-        if(D) {Log.d(DEBUG_TAG,"Updating alarm switch state");}
-        boolean alarmSet = PrefUtil.getBoolean(getActivity(), PrefKey.ALARM_SET, false);
-        switch_alarm.setChecked(alarmSet);
-    }
-
-
     /**
      * Update the alarm state view and preference
      */
-    private void updateAlarmStateFromPreference(){
-        if(D) {Log.d(DEBUG_TAG,"Getting alarm state");}
-        AlarmGson alg = PrefUtil.getAlarmTimeGson(getActivity());
+    private void loadAlarmState(){
+        if(D) {Log.d(DEBUG_TAG,"Updating alarm state from preference.");}
+        AlarmGson alg = PrefUtil.getAlarmGson(fragmentContext);
         if(alg != null){
+            if(D) {Log.d(DEBUG_TAG,"Alarm state found. Preparing views.");}
             String alarmFormatted = StringUtil.getAlarmTimeFormatted(alg.getHour(),alg.getMinute());
             textView_alarmTime.setText(alarmFormatted);
-            mCalendarSet = AlarmUtil.getNextAlarmTimeAbsolute(alg.getHour(), alg.getMinute());
+            setCircleButtonActive(alg.isAlarmSet());
         } else {
-            if(D) {Log.d(DEBUG_TAG,"No alarm state found");}
+            if(D) {Log.d(DEBUG_TAG,"No alarm state found.");}
         }
+    }
+
+    private void activateAlarm(){
+        AlarmGson alg = PrefUtil.getAlarmGson(fragmentContext);
+        alg.setAlarmSet(true);
+        PrefUtil.setAlarmGson(fragmentContext, alg);
+        mCalendarSet = AlarmUtil.getNextAlarmTimeAbsolute(alg.getHour(), alg.getMinute());
+        AlarmUtil.setAlarm(fragmentContext, mCalendarSet);
+        setCircleButtonActive(true);
+    }
+
+    private void deactivateAlarm(){
+        AlarmGson alg = PrefUtil.getAlarmGson(fragmentContext);
+        alg.setAlarmSet(false);
+        PrefUtil.setAlarmGson(fragmentContext, alg);
+        AlarmUtil.deactivateAlarm(fragmentContext);
+        setCircleButtonActive(false);
     }
 
     //================================================================================
     // UI
     //================================================================================
 
-    private void setSwitchCheckedListener(){
-        switch_alarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                PrefUtil.putBoolean(getActivity(), PrefKey.ALARM_SET, isChecked);
-                if (isChecked) {
-                    if(mCalendarSet != null){
-                        AlarmUtil.setAlarm(getActivity(), mCalendarSet);
-                    } else {
-                        Toast.makeText(getActivity(), R.string.toast_alarmTimeNotSet,
-                                Toast.LENGTH_SHORT).show();
-                        buttonView.setChecked(false);
-                    }
-                } else {
-                    AlarmUtil.deactivateAlarm(getActivity());
-                }
-            }
-        });
+    private void setCircleButtonActive(boolean isActive){
+        if(isActive){
+            circleButton.setColor(getResources().getColor(R.color.yellow_main));
+            circleButton.setImageResource(R.drawable.ic_action_alarmclock_light);
+        } else {
+            circleButton.setColor(getResources().getColor(R.color.veryLightGray));
+            circleButton.setImageResource(R.drawable.ic_alarmclock_light_no_bells);
+        }
+
     }
 
     /**
      * Show a timepicker to set the alarm time
      */
     private void showTimePicker() {
-        if(D) {Log.d(DEBUG_TAG,"Showing timpicker dialog");}
+        if(D) {Log.d(DEBUG_TAG,"Showing time picker dialog.");}
         TimePickerBuilder tpb = new TimePickerBuilder()
                 .setFragmentManager(getChildFragmentManager())
                 .setStyleResId(R.style.BetterPickersDialogFragment_Light)
@@ -164,11 +193,13 @@ public class SetAlarmFragment extends Fragment implements
 
     @Override
     public void onDialogTimeSet(int reference, int hourOfDay, int minute) {
-        if(D) {Log.d(DEBUG_TAG,"Timepicker finished. Setting alarm time.");}
-        String alarmTime = StringUtil.getAlarmTimeFormatted(hourOfDay, minute);
-        textView_alarmTime.setText(alarmTime);
-        mCalendarSet = AlarmUtil.getNextAlarmTimeAbsolute(hourOfDay,minute);
-        PrefUtil.updateAlarmTime(getActivity(), hourOfDay, minute);
-        switch_alarm.setChecked(true);
+        if(D) {Log.d(DEBUG_TAG,"Time picker finished. Setting alarm time.");}
+        String alarmTimeFormatted = StringUtil.getAlarmTimeFormatted(hourOfDay, minute);
+        textView_alarmTime.setText(alarmTimeFormatted);
+        AlarmGson alg = PrefUtil.getAlarmGson(fragmentContext);
+        alg.setHour(hourOfDay);
+        alg.setMinute(minute);
+        PrefUtil.setAlarmGson(fragmentContext, alg);
+        activateAlarm();
     }
 }
