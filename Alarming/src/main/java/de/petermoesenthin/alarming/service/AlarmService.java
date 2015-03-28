cd8438722324094dc3e58f7656ac3f2d47901368
Peter Mösenthin <peter.moesenthin@gmail.com>
2015-03-28 14:47:21 +0100

    update copyright notice

--- Alarming/src/main/java/de/petermoesenthin/alarming/service/AlarmService.java
+++ Alarming/src/main/java/de/petermoesenthin/alarming/service/AlarmService.java
@@ -1,5 +1,5 @@
 package de.petermoesenthin.alarming.service;/*
- * Copyright (C) 2014 Peter Mösenthin <peter.moesenthin@gmail.com>
+ * Copyright (C) 2014-2015 Peter Mösenthin <peter.moesenthin@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
