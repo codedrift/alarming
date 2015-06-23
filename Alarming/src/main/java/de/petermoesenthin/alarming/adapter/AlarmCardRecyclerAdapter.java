b8f5826fa7dd27e5bcd458ab41bb2ff84d71efa7
Peter Mösenthin <moesenthin@sipgate.de>
2015-06-23 17:06:35 +0200

    switch to gplv3

--- Alarming/src/main/java/de/petermoesenthin/alarming/adapter/AlarmCardRecyclerAdapter.java
+++ Alarming/src/main/java/de/petermoesenthin/alarming/adapter/AlarmCardRecyclerAdapter.java
@@ -1,3 +1,21 @@
+/*
+ * Alarming, an alarm app for the Android platform
+ *
+ * Copyright (C) 2014-2015 Peter Mösenthin <peter.moesenthin@gmail.com>
+ *
+ * Alarming is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.petermoesenthin.alarming.adapter;
 
 import android.content.Context;
@@ -17,9 +35,6 @@ import de.petermoesenthin.alarming.util.StringUtil;
 
 import java.util.List;
 
-/**
- * Created by Peter Mösenthin.
- */
 public class AlarmCardRecyclerAdapter extends RecyclerView.Adapter<AlarmCardRecyclerAdapter.AlarmCardViewHolder>{
 
 	public static final String DEBUG_TAG = AlarmCardRecyclerAdapter.class.getSimpleName();
